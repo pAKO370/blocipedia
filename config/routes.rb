9a32aef36a45d3a9fb3f9b4d57f7f2f891cf76bb
Phil Fluegel <pfluegelcx@gmail.com>
2015-12-15 14:55:11 -0600

    New view,new controller,alerts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+  get 'wikis/index'
+
+  get 'wikis/show'
+
+  get 'wikis/new'
+
+  get 'wikis/edit'
+
   devise_for :users
+
+  resources :wikis
+
   get 'welcome/index'
 
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
 
   root 'welcome#index'
 
